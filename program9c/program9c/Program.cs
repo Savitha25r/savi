75910209885ea6142d3ca383c80a9b6794482392
Savitha25r <114404662+Savitha25r@users.noreply.github.com>
2022-09-27 09:07:23 -0700

    Add files via upload
    program9c(C# to accept two words from user and find out if they are same. )

---
+++ program9c/program9c/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class program9c
+{
+
+    // Driver code
+    public static void Main()
+    {
+
+        // Get the strings which is to be checked
+        String string1 = Console.ReadLine();
+        Console.WriteLine("Enter the first string: "
+                          + string1);
+
+        // Get the strings which is to be checked
+        String string2 = Console.ReadLine();
+        Console.WriteLine("Enter the second string :"
+                          + string2);
+
+        // Check if both strings are equal
+        Console.WriteLine("\nAre both strings same: ");
+
+        if (string1.Equals(string2) == true)
+        {
+            Console.WriteLine("Yes");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("No");
+            Console.ReadLine();
+        }
+    }
+}
