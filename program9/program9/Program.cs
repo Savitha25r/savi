5d015943011859ca6c3edf10ef5b0ce5ddfd8d94
Savitha25r <114404662+Savitha25r@users.noreply.github.com>
2022-09-27 08:44:41 -0700

    Add files via upload
    program9 (palindrome)

---
+++ program9/program9/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace program9
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string to Check Palindrome : ");
+            string name = Console.ReadLine();
+            string reverse = string.Empty;
+
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                reverse += name[i];
+            }
+
+            if (name == reverse)
+            {
+                Console.WriteLine($"{name} is Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} is not Palindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+
+/*using System;
+public class program9
+{
+    public static void Main(string[] args)
+    {
+        int no, total = 0, t, rev;
+        Console.Write("Please provide the no: ");
+        no = int.Parse(Console.ReadLine());
+        t = no;
+        while (no > 0)
+        {
+            rev = no % 10;
+            total = (total * 10) + rev;
+            no = no / 10;
+        }
+        if (t == total)
+            Console.Write("The given number is Palindrome.");
+        else
+            Console.Write("The given number is not Palindrome");
+        Console.ReadLine();
+    }
+}*/
