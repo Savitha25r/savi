81be5c36b091eff5bd82dd35f2e53ad4c5f13638
Savitha25r <114404662+Savitha25r@users.noreply.github.com>
2022-09-27 20:07:50 -0700

    Add files via upload
    C# to accept a word from the user and display the length of it(program8a)

---
+++ program8a/program8a/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace program8a
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string : ");
+            string str = Console.ReadLine();
+            int len = str.Length;
+            Console.WriteLine("Length of the string is : " + len);
+            Console.ReadLine();
+        }
+    }
+}
