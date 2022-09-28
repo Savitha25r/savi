63b5d353065e492b1d6afacfa8718f19f146bfe6
Savitha25r <114404662+Savitha25r@users.noreply.github.com>
2022-09-27 20:00:13 -0700

    Add files via upload
    program in C# to accept ten marks and display the descending order

---
+++ program7f/program7f/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace program7f
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[] a = new int[20];
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine("enter the Marks" + " " + (i + 1) + ":");
+                a[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 9; j++)
+                {
+                    if (a[j] < a[j + 1])
+
+                    {
+                        int temp = a[j + 1];
+                        a[j + 1] = a[j];
+                        a[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Descending order:");
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine(a[i] + "");
+            }
+            Console.ReadKey();
+        }
+    }
+}
