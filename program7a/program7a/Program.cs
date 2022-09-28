89e3196439e005f1d9b9c2bc116e0098d9c061a2
Savitha25r <114404662+Savitha25r@users.noreply.github.com>
2022-09-27 19:56:46 -0700

    Add files via upload
    program 7 a(program in C# to accept ten marks and display the total)

---
+++ program7a/program7a/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;  
+public class program7a
+{
+    public static void Main()
+    {
+        int i, n, total = 0;
+       
+
+        Console.Write("\n\n");
+        Console.Write("Read 10 numbers and calculate sum and average:\n");
+        Console.Write("----------------------------------------------");
+        Console.Write("\n\n");
+
+        Console.Write("Input the 10 numbers : \n");
+        for (i = 1; i <= 10; i++)
+        {
+            Console.Write("Number-{0} :", i);
+
+            n = Convert.ToInt32(Console.ReadLine());
+            total += n;
+        }
+       
+        Console.Write("The total of 10 no is : {0}\n", total);
+        Console.ReadLine();
+    }
+}
